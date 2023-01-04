9ff334b7d9b2aa6d78fb981c56a2456e16c16b8c
Ranjeethkumar <rmpranjeeth@gmail.com>
2023-01-04 23:29:49 +0530

    css

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -65,18 +65,18 @@ const Container = styled.div`
     color: white;
     gap: 1rem;
     .emoji {
-      position: relative;
+      position: relative !important;
       svg {
         font-size: 1.5rem;
         color: #ffff00c8;
         cursor: pointer;
       }
       .emoji-picker-react {
-        position: absolute;
-        top: -350px;
-        background-color: #080420;
-        box-shadow: 0 5px 10px #9a86f3;
-        border-color: #9a86f3;
+        position: absolute !important;
+        top: -350px !important;
+        background-color: #080420 !important;
+        box-shadow: 0 5px 10px #9a86f3 !important;
+        border-color: #9a86f3 !important;
         .emoji-scroll-wrapper::-webkit-scrollbar {
           background-color: #080420;
           width: 5px;
